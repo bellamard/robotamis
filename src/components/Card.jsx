97c3c9f7ac4680cccd775ed372c2e789e1d3bd4f
bellamard <belamard@gmail.com>
2021-05-03 00:45:37 +0100

    commit final

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
-function Card({robot}) {
+function Card({ robot }) {
 
-    
-    const {name,email, id, username, phone,website}=robot;
+
+    const { name, email, id, username, phone, website } = robot;
     return (
-        
-        <div className='card' >
-            <img src={`https://robohash.org/${id}`} alt={name}/>
-            <h3>{name}</h3>
-            <span>{email}</span>
-            <Link to={"/Details/"+id+"/"+name+"/"+username+"/"+phone+"/"+website+"/"+email} className='lien'>Plus d'infos</Link>
+        <div>
+            <div className='card' >
+                <img src={`https://robohash.org/${id}`} alt={name} />
+                <h3>{name}</h3>
+                <span>{email}</span>
+                <Link to={"/Details/" + id + "/" + name + "/" + username + "/" + phone + "/" + website + "/" + email} className='lien'>Plus d'infos</Link>
+            </div>
         </div>
-        
+
     );
 }
 
